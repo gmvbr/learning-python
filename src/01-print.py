ceea54443db5c753d9d258c85e26699c70073cb3
gmvbr <80280612+gmvbr@users.noreply.github.com>
2021-03-17 15:44:54 -0400

    Basico - 01 - Print

---
+++ src/01-print.py
@@ -0,0 +1,11 @@
+# Resultado Hello World
+print("Hello World")
+
+# Resultado Hello World Python
+print("Hello World", "Python")
+
+# Resultado Hello World, Python
+print("Hello World", "Python", sep=',')
+
+# Resultado Teste-01-Print
+print("Teste", "01", "Print", sep="-")
