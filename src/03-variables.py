2c88f1bca6af4a3dfb2a19b76c4b9d015ef3e288
gmvbr <80280612+gmvbr@users.noreply.github.com>
2021-03-17 16:25:23 -0400

    Adicionado variáveis

---
+++ src/03-variables.py
@@ -0,0 +1,34 @@
+# Definir variáveis
+x = 5
+y = 20
+
+# operação basica
+soma = x + y
+print(soma)
+
+# operação mais avançada
+resultado = x + (y * 5) % 2
+print(resultado)
+
+# int -> str
+ano = str(2021)
+print(type(ano))
+print(ano)
+
+# str -> int
+age = int('21')
+print(type(age))
+print(age)
+
+# str -> float
+versao = float('3.6')
+print(type(versao))
+print(versao)
+
+# Verificar o tipo da variável
+tipo = type(x)
+print(tipo)
+
+# str
+name = 'variables'
+print(name)
