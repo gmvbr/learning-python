23cf6b05d6c93d02edca2b2b6893a109afbf7cf2
gmvbr <80280612+gmvbr@users.noreply.github.com>
2021-03-17 16:41:18 -0400

    Basico - 04 - String

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Básico em python
 - [01 - Comando print](src/01-print.py)
 - [02 - Sintaxe](src/02-syntax.py)
 - [03 - Variáveis](src/03-variables.py)
+- [04 - String](src/03-strings.py)
---
+++ src/04-strings.py
@@ -0,0 +1,42 @@
+from string import Template
+
+# Juntar strings
+cor = "Azul"
+corDoCarro = "Carro " + cor
+print(corDoCarro)
+
+# Formatação de string
+cor = "Verde"
+corDoCarro = "Carro %s" % cor
+print(corDoCarro)
+
+# Formatação de string outro estilo
+cor = "Laranja"
+corDoCarro = "Carro {}".format(cor)
+print(corDoCarro)
+
+# Interpolação String f-String (Python 3.6)
+cor = "Laranja"
+corDoCarro = f"Carro {cor}"
+print(corDoCarro)
+
+# Usando Template
+helloTemplate = Template("Hello World, $lang")
+hello = helloTemplate.substitute(lang="Python")
+print(hello)
+
+
+base = "Teste"
+
+# length
+print(len(base))
+
+# split
+base = "Mesa Cadeira Computador Notebook Tablet Mouse Teclado"
+print(base.split(' '))
+
+# Strip
+print(base.strip('MesaTeclado'))
+
+# Index of
+print(base.index('Cadeira'))
