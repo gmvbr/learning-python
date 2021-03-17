279ed62e318812be642ec6f85484f64ad7b4a407
gmvbr <80280612+gmvbr@users.noreply.github.com>
2021-03-17 19:08:25 -0400

    Melhorias em 04-strings.py

--- src/04-strings.py
+++ src/04-strings.py
@@ -1,29 +1,24 @@
 from string import Template
 
-# Juntar strings
+# combinar strings
 cor = "Azul"
-corDoCarro = "Carro " + cor
-print(corDoCarro)
+print("Carro " + cor)
 
 # Formatação de string
 cor = "Verde"
-corDoCarro = "Carro %s" % cor
-print(corDoCarro)
+print("Carro %s" % cor)
 
 # Formatação de string outro estilo
 cor = "Laranja"
-corDoCarro = "Carro {}".format(cor)
-print(corDoCarro)
+print("Carro {}".format(cor))
 
-# Interpolação String f-String (Python 3.6)
+# Interpolação String f-String (Python 3.6+)
 cor = "Laranja"
-corDoCarro = f"Carro {cor}"
-print(corDoCarro)
+print(f"Carro {cor}")
 
 # Usando Template
-helloTemplate = Template("Hello World, $lang")
-hello = helloTemplate.substitute(lang="Python")
-print(hello)
+template = Template("Hello World, $lang")
+print(template.substitute(lang="Python"))
 
 # length
 base = "Teste"
@@ -38,3 +33,27 @@ print(base.strip('MesaTeclado'))
 
 # Index of
 print(base.index('Cadeira'))
+
+# Slicing
+print(base[0:4])  # de x para y
+print(base[:4])  # Do começo para y
+print(base[5:])  # x do final para o começo
+
+# Posição -5 do final para o começo
+# Posição -2 do final para o começo
+print(base[-7:-2])
+
+# Maiúsculas
+print(base.upper())
+
+# Minúsculas
+print(base.lower())
+
+# Remover espaço do inicio ou fim
+print(" Olá mundo  ".strip())
+
+# Substituir String
+print("Olá mundo".replace("mundo", "programador"))
+
+# Dividir String
+print("Olá-mundo".split('-'))
